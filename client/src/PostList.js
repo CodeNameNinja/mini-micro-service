82d706d638c2e7aa10a836a1c7166a0799fd68e5
Mitchell <mwhyuen@gmail.com>
2022-01-15 10:44:25 +0200

    feat: ADD commentCreate component

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import CommentCreate from "./CommentCreate";
 const PostList = () => {
   const [posts, setPosts] = useState({});
   const fetchPosts = async () => {
@@ -22,11 +22,16 @@ const PostList = () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
   });
-  return <div className="d-flex flex-row flex-wrap justify-content-between">{renderPosts}</div>;
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderPosts}
+    </div>
+  );
 };
 
 export default PostList;
